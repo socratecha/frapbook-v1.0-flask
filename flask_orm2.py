38deb8af60709f57fec440a2b20b754c533d6b2c
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:41:12 -0500

    Data repair scripting

--- flask_orm2.py
+++ flask_orm2.py
@@ -11,6 +11,7 @@ connection_string = "sqlite:///checksdemo.db"
 db   = create_engine(connection_string)
 base = declarative_base()
 
+standard_skills = ['designer', 'client', 'server', 'db_admin']
 
 class Employee(base):
     __tablename__ = 'employees'
@@ -26,6 +27,10 @@ class Project(base):
     needs = Column(types.String(length=120))
     members = relationship("Assignment", back_populates="project")
                            
+    def nonstandard_needs(self):
+        return [need for need in self.needs.split()
+                if need not in standard_skills]
+
 class Assignment(base):
      __tablename__ = 'assignments'
      id = Column(types.Integer, primary_key=True)
@@ -42,3 +47,33 @@ Session = sessionmaker(db)
 session = Session()
 
 base.metadata.create_all(db)
+def add_sample_data():
+    # Add employees and projects; assign employees to projects
+    session.add_all([
+        Employee(id=1, name="Hercules the Bear", skill="client"),
+        Employee(id=2, name="Herman Hollerith", skill="db_admin"),
+        Employee(id=3, name="Martina Hingis", skill="server"),
+        Employee(id=4, name="Margo Hoff", skill="designer"),
+        Employee(id=5, name="Hulk Hogan"),
+        Project(id=1, name="Design Transmogrifier",
+                needs='designer server'),
+        Project(id=2, name="Build Duplicator", needs='client db_admin'),
+        Project(id=3, name="Repair Enhance-o-Tron",
+                needs='client juggler'),
+        Assignment(employee_id=1, project_id=2),
+        Assignment(employee_id=2, project_id=2),
+        Assignment(employee_id=3, project_id=3),
+        Assignment(employee_id=4, project_id=1),
+    ])
+    session.commit()
+
+def clean_data():
+    for project in session.query(Project).all():
+        nonstandards = project.nonstandard_needs()
+        if nonstandards:
+            print('Project {} requires non-standard skills: {}'.format(
+                project.name, ', '.join(nonstandards)))
+
+if __name__ == '__main__':
+    add_sample_data()
+    clean_data()
