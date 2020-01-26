56001a74201d00aeb7e296e310f6e91453ecd7a7
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:41:09 -0500

    postgres examples

--- flask_orm.py
+++ flask_orm.py
@@ -49,10 +49,27 @@ session.commit()
 newbook = Book(author="Annie Dillard", title="Teaching a Stone to Talk", available=True)
 session.add(newbook)
 session.commit()
-newyear = BirthYear(author="Perfect Vision", birth_year=2020)
-session.add(newyear)
+class LifeSpan(base):
+    __tablename__ = 'life_spans'
+    name = Column(types.String(length=50), primary_key=True)
+    birth_year = Column(types.Integer,
+                        CheckConstraint('birth_year < 2020'),
+                        nullable=False)
+    death_year = Column(types.Integer)
+    __table_args__ = (
+        CheckConstraint('(death_year is NULL) or (death_year >= birth_year)'),
+    )
+
+base.metadata.create_all(db)
+ls1 = LifeSpan(name="Gian-Carlo Rota", birth_year=1932, death_year=1999)
+session.add(ls1)
+session.commit()
+
+ls2 = LifeSpan(name="Benjamin Button", birth_year=2008, death_year=1922)
+session.add(ls2)
 try:
     session.commit()
+    print('Success!')
 except sqlalchemy.exc.IntegrityError as e:
-    print("Integrity violation blocked!")
+    print('Invalid ages: integrity violation blocked')
     session.rollback()
