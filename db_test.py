627666ae804a9ec162eee76128516799982be561
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:41:19 -0500

    Data pytesting

--- db_test.py
+++ db_test.py
@@ -11,6 +11,13 @@ def db_session_clean():
     clean_data()        
     yield session
     
+@pytest.fixture(scope='module')
+def db_session_unclean():
+    '''Creates a database and populates it with unclean data'''
+    drop_tables()
+    add_sample_data()
+    yield session    
+
 def test_clean_nonstandard_skills(db_session_clean):
     '''Check project required skills are from the standard list'''
     for project in db_session_clean.query(Project).all():
@@ -21,3 +28,14 @@ def test_clean_needless_assignments(db_session_clean):
     for employee in db_session_clean.query(Employee).all():
         assert not employee.needless_assignments()
 
+def test_unclean_nonstandard_skills(db_session_unclean):
+    '''Check that unclean data has a nonstandard project skills'''
+    with pytest.raises(AssertionError) as e:
+        for project in db_session_unclean.query(Project).all():
+            assert not project.nonstandard_needs()
+
+def test_unclean_needless_assignments(db_session_unclean):
+    '''Check unclean data has a project member that cannot help'''
+    with pytest.raises(AssertionError) as e:
+        for employee in db_session_unclean.query(Employee).all():
+            assert not employee.needless_assignments()
