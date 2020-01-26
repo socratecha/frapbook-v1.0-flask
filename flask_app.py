58d1e3f70468ca45f786287c0034b79411812de6
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:32:21 -0500

    Second flask app + decorator example

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/one")
+def favorite():
+    return "Thank you!"
+
+@app.route("/one/triceratops")
 @app.route("/")
 def hello():
     return "Hello World!"
