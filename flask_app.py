6e8c9544f10810d11b266e835927f960790c6ce2
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:32:38 -0500

    Third flask app

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/one")
-def favorite():
-    return "Thank you!"
-
-@app.route("/one/triceratops")
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/books/<book_id>")
+def abook(book_id):
+    return "I think you'll enjoy {}".format(book_id)
