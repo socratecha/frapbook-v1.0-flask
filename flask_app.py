1e4bb5a5570d8c3b420dfb7f537b77c076758408
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:33:06 -0500

    First flask-restplus api

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,35 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restplus import Resource, Api
 
-@app.route("/books/<book_id>")
-def abook(book_id):
-    return "I think you'll enjoy {}".format(book_id)
+app = Flask(__name__)
+api = Api(app)
+
+numbers = [
+    { 'id':1, 'number':28, 'description':'2nd perfect number' },
+    { 'id':2, 'number':51, 'description':"Looks prime but isn't" },
+    { 'id':3, 'number':28, 'description':'7th triangle number' },
+]
+top_number = 3
+
+
+@api.route('/my-api')
+class Numbers(Resource):
+    def get(self):
+        '''Get the full list of favorite numbers'''
+        return numbers
+    
+    def post(self):
+        '''Post a new favorite number; server assigns the id'''
+        global top_number
+        top_number += 1
+        numbers.append( {'id':top_number,
+                         'number':api.payload['number'],
+                         'description':api.payload['description']} )
+        return top_number
+    
+@api.route('/my-api/<int:id>')
+class OneNumber(Resource):
+    def get(self, id):
+        '''Get a single favorite number selected by its id'''
+        matches = [ number for number in numbers if number['id'] == id ]
+        return matches[0]
