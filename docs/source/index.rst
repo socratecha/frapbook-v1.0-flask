fe117d810efa3269dfe0bda2cd16b94b2061389e
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:34:28 -0500

    added sphinx doc to flask examples

---
+++ docs/source/index.rst
@@ -0,0 +1,26 @@
+.. Flask Examples documentation master file, created by
+   sphinx-quickstart on Sat Jan 25 21:33:56 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Flask Examples's documentation!
+==========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Numbers API
+===========
+The numbers API allows you to track why you like different numbers.
+
+.. automodule:: flask_app
+   :members:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,19 @@
+'''
+.. epigraph::
+
+    *It’s like asking why is Ludwig van Beethoven’s Ninth Symphony 
+    beautiful. If you don't see why, someone can't tell you. I know 
+    numbers are beautiful. If they aren't beautiful, nothing is.*
+    -- Paul Erdos
+
+The Numbers API is for when you *really* must track your **favorite**
+**numbers**.
+
+.. warning::
+   This implementation does not persist data across sessions. Also, if
+   it is run with multiple server processes, their data will not
+   synchronize.
+'''
 from flask import Flask
 from flask_restplus import Resource, Api
 from flask_restplus import fields
@@ -20,12 +36,36 @@ number_model = api.model('Favorite Number', {
 @api.route('/my-api')
 class Numbers(Resource):
     def get(self):
-        '''Get the full list of favorite numbers'''
+        '''Get the full list of favorite numbers
+
+        :route: ``/my-api`` GET
+        :return: 
+           A list of favorite numbers, each with:
+              * ``id`` a unqiue int identifier
+              * ``number`` the favorite number
+              * ``description`` the reason it is a favorite
+        '''
         return numbers
     
     @api.expect(number_model)
-    def post(self):
-        '''Post a new favorite number; server assigns the id'''
+    def post(self):        
+        '''Post a new favorite number; server assigns the id
+
+        Appends a favorite number with a description into the
+        collection, assigning it a new unique identifier in the
+        collection. Returns the new identifier.
+
+        :route: ``/my-api`` POST
+        :payload:
+           Submit a JSON object with:
+              * ``number``: the new favorite number (int)
+              * ``description``: reason it's a favorite (str)
+
+        :error: **400** if missing either ``number`` or ``description`` field
+        :return: Newly assigned identifier
+        '''
+        if not ('number' in api.payload and 'description' in api.payload):
+            api.abort(400, "Payload must include number and description")
         global top_number
         top_number += 1
         numbers.append( {'id':top_number,
@@ -36,7 +76,23 @@ class Numbers(Resource):
 @api.route('/my-api/<int:id>')
 class OneNumber(Resource):
     def get(self, id):
-        '''Get a single favorite number selected by its id'''
+        '''Get a single favorite number selected by its id
+        
+        Gets both the number and the description of why its a
+        favorite number if the id is valid, otherwise produces an
+        error code. 
+
+        :route: ``/my-api/<int:id>`` GET
+        :param id: favorite number identifier
+        :type id: int
+
+        :error: **404** if ``id`` is not in the collection
+        :return: 
+           A single favorite number with:
+              * ``id`` a unqiue int identifier
+              * ``number`` the favorite number
+              * ``description`` the reason it is a favorite
+        '''
         matches = [ number for number in numbers if number['id'] == id ]
         if len(matches) == 0:
             api.abort(404, "Unique id {} not found".format(id))
