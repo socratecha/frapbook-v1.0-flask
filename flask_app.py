5b086ad3afa1dbd667dd1c59652190d27274c1c4
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:33:20 -0500

    Added 404 error

--- flask_app.py
+++ flask_app.py
@@ -38,4 +38,6 @@ class OneNumber(Resource):
     def get(self, id):
         '''Get a single favorite number selected by its id'''
         matches = [ number for number in numbers if number['id'] == id ]
+        if len(matches) == 0:
+            api.abort(404, "Unique id {} not found".format(id))
         return matches[0]
